d3448bd0f2d6f977320fafd084eb57afa655d6db
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2023-12-12 16:21:59 +0100

    fix in build.js

--- scripts/build.js
+++ scripts/build.js
@@ -15,20 +15,21 @@ process.on("unhandledRejection", (err) => {
 require("../config/env");
 
 const path = require("path");
-const chalk = require("react-dev-utils/chalk");
 const fs = require("fs-extra");
 const webpack = require("webpack");
-const {
-  measureFileSizesBeforeBuild,
-  printFileSizesAfterBuild,
-  FileSizeReporter,
-  printHostingInstructions,
-  printBuildError,
-  checkRequiredFiles,
-  formatWebpackMessages,
-  browsersHelper: { checkBrowsers },
-} = require("react-dev-utils");
 
+const chalk = require("react-dev-utils/chalk");
+const checkRequiredFiles = require("react-dev-utils/checkRequiredFiles");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
+const printHostingInstructions = require("react-dev-utils/printHostingInstructions");
+const FileSizeReporter = require("react-dev-utils/FileSizeReporter");
+const printBuildError = require("react-dev-utils/printBuildError");
+const measureFileSizesBeforeBuild =
+  FileSizeReporter.measureFileSizesBeforeBuild;
+const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
+const { checkBrowsers } = require("react-dev-utils/browsersHelper");
+
+const paths = require("../config/paths");
 const {
   appHtml,
   appIndexJs,
